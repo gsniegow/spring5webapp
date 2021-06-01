af3a2b109c3d0b4bce4cdee2ca374b18402153b9
gsnie <gsniegow@gmail.com>
2021-06-01 14:53:47 -0500

    Updated Lab

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -24,17 +24,26 @@ public class BootStrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        Publisher p1 = new Publisher("Gene Sniegowski", "123 Main St", "Abywhere", "IL", "11111");
+        publisherRepository.save(p1);
 
         Author a1 = new Author("Gene", "Sniegowski");
         Book b1 = new Book("My Book", "111111111");
+        a1.getBooks().add(b1);
+        b1.setPublisher(p1);
+        bookRepository.save(b1);
+
+        publisherRepository.save(p1);
+
+        p1.getBooks().add(b1);
 
         a1.getBooks().add(b1);
         b1.getAuthors().add(a1);
 
+        publisherRepository.save(p1);
         authorRepository.save(a1);
         bookRepository.save(b1);
 
-        Publisher p1 = new Publisher("Gene Sniegowski", "123 Main St", "Abywhere", "IL", "11111");
         publisherRepository.save(p1);
 
         System.out.println("Started in Bootstrap");
--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -16,6 +16,17 @@ public class Book {
     private String title;
     private String isbn;
 
+    @ManyToOne
+    private Publisher publisher;
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
+    }
+
     @ManyToMany
     @JoinTable(name="author_book",
                joinColumns = @JoinColumn(name="book_id"),
